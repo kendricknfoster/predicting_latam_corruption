2b0fe0fc4d99939f6173f80df2498aab56bae800
Kendrick Foster <your@email.com>
2020-11-11 20:42:04 -0600

    Add shapefile and work on ShinyApp.

--- app.R
+++ app.R
@@ -1,49 +1,78 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+
+# Load relevant libraries. 
 
 library(shiny)
+library(shinythemes)
+library(tidyverse)
+
+CPI_shapefile <- readRDS("CPI_shapefile.RDS")
+final_data <- readRDS("final_data.RDS")
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+ui <- shinyUI(
+    navbarPage(
+        theme = shinytheme("readable"),
+        "Predicting Corruption in Latin America",
+        
+        tabPanel("The Question",
+                 titlePanel("How can we predict corruption in Latin America?"),
+                 p("Latin America is generally regarded as one of the most corrupt regions 
+                   in the world. However, there is a lot of variation within the region: 
+                   Uruguay, Chile, and Costa Rica, for instance, are regarded as some of the 
+                   least corrupt countries in the world. Why is there so much variation
+                   within the region?"),
+                 plotOutput("cpi_shapefile")),
+        
+        tabPanel("The Data",
+            fluidPage(
+                selectInput("x", 
+                            "Select explanatory variable",
+                            choices = names(final_data)),
+                selectInput("y", "Select dependent variable",
+                            choices = names(final_data)),
+                selectInput("geom", "geom", c("point", "smooth")), 
+                plotOutput("plot"))),
+        
+        tabPanel("The Model"),
+        
+        tabPanel("About",
+                 titlePanel("About"),
+                 h3("The Data"),
+                 p(tags$ol(
+                     tags$li(
+                         "GDP per capita, inequality, government spending, 
+                         and poverty all came from the", a("The World Bank", href = "https://data.worldbank.org"), 
+                         "dataset. The inequality data is a lot spottier than the other three indicators."
+                     )
+                 )
+                   ))
+        
+        
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$cpi_shapefile <- renderPlot({
+        CPI_shapefile %>%
+        select(country, CPI, geometry) %>%
+            ggplot(aes(geometry = geometry)) +
+            geom_sf(aes(fill = CPI)) +
+            scale_fill_distiller(palette = "Reds")
+    }, height = 400, width = 600)
+    
+    plot_geom <- reactive({
+        switch(input$geom,
+               point = geom_point(),
+               smooth = geom_smooth(method = lm)
+        )
     })
+    
+    output$plot <- renderPlot({
+        ggplot(final_data, aes(.data[[input$x]], .data[[input$y]])) +
+            plot_geom()})
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
---
+++ regressions.R
@@ -0,0 +1,21 @@
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+
+stan_glm(data = final_data,
+         CPI ~ `GDP Per Capita`*`Gini`*`Poverty Rate` + `Economic Freedom` + `Public Campaign Finance` + `Bureaucratic Remuneration` + `Government Spending` + `Infrastructure Spending`,
+         refresh = 0) %>%
+  print(digits = 5)
+
+robust_model <- rlm(CPI ~ `GDP Per Capita`,
+                    data = final_data)
+
+tree <- rpart(CPI ~ `GDP Per Capita` + `Gini` + `Poverty Rate` + `Economic Freedom` + `Public Campaign Finance` + `Bureaucratic Remuneration` + `Government Spending` + `Infrastructure Spending`,
+              data = final_data,
+              cp = 0.1)
+
+rpart.plot(tree,
+           type = 2)
+
+rf <- randomForest(CPI ~ `GDP Per Capita` + `Gini` + `Poverty Rate` + `Economic Freedom` + `Public Campaign Finance` + `Bureaucratic Remuneration`,
+                   data = final_data)
