91e89944ac181329a82ced58cc195d5710743ca5
Kendrick Foster <your@email.com>
2020-11-27 20:14:02 -0600

    Read in more CPI data and improve Shiny UI.

--- app.R
+++ app.R
@@ -12,47 +12,83 @@ final_data <- readRDS("final_data.RDS")
 
 ui <- shinyUI(
     navbarPage(
-        theme = shinytheme("readable"),
+        theme = shinytheme("cosmo"),
         "Predicting Corruption in Latin America",
         
         tabPanel("The Question",
                  titlePanel("How can we predict corruption in Latin America?"),
-                 p("Latin America is generally regarded as one of the most corrupt regions 
+                 fluidPage(
+                     fluidRow(column(6,
+                                     plotOutput("cpi_shapefile")),
+                              column(6, 
+                                     p("Latin America is generally regarded as one of the most corrupt regions 
                    in the world. However, there is a lot of variation within the region: 
                    Uruguay, Chile, and Costa Rica, for instance, are regarded as some of the 
                    least corrupt countries in the world. Why is there so much variation
                    within the region?"),
-                 plotOutput("cpi_shapefile")),
+                                     p("Political science has posited several major explanations for corruption: ",
+                                       tags$ul(
+                                           tags$li("GDP Per Capita: Richer countries
+                                                   have lower levels of corruption due to more
+                                                   developed political and economic institutions (Serra 2006)."),
+                                           tags$li("Poverty"),
+                                           tags$li("Inequality"))),
+                                     p("I also hope to test several less common explanations for corruption using datasets beyond traditional World Bank datasets: ",
+                                       tags$ul(
+                                           tags$li("Bureaucratic Remuneration: 
+                                                   If bureaucrats get paid more, they have less incentive for petty corruption."),
+                                           tags$li("Government Spending: 
+                                                   If a government spends less overall, there is less of a chance for grand corruption to occur."),
+                                           tags$li("Infrastructure Spending: 
+                                                   Infrastructure projects are fertile breeding grounds for corruption, so a we would expect a higher level of corruption with more spending on infrastructure."),
+                                           tags$li("Public Campaign Finance: ")),
+                                     p("I do not include two common indicators 
+                                       for corruption since the CPI calculation 
+                                       already includes them to derive the CPI score: ")))
+                     )
+                 )), 
         
         tabPanel("The Data",
             fluidPage(
                 selectInput("x", 
                             "Select explanatory variable",
-                            choices = names(final_data)),
-                selectInput("geom", "geom", c("point", "linear", "quadratic")), 
+                            choices = c("GDP per Capita" = "gdp_pc",
+                                        "Gini Coefficient" = "gini",
+                                        "Poverty Rate" = "poverty_rate",
+                                        "Government Spending" = "govt_spending",
+                                        "Poverty Rate" = "poverty_rate",
+                                        "Bureaucratic Remuneration" = "bur_rem",
+                                        "Public Campaign Finance" = "pcf")),
+                selectInput("geom", 
+                            "Select plot type", 
+                            c("Scatterplot" = "point", 
+                              "Linear Regression" = "linear", 
+                              "Quadratic Regression" = "quadratic")), 
                 plotOutput("plot"))),
         
         tabPanel("The Model"),
         
         tabPanel("About",
                  titlePanel("About"),
+                 h3("Project Background"),
+                 p(),
+                 p("The GitHub repo for this project lives", a("here", href = "https://github.com/kendricknfoster/predicting_latam_corruption", ".")),
                  h3("The Data"),
-                 p(tags$ol(
-                     tags$li("CPI data"),
+                 p(tags$ul(
+                     tags$li("CPI data comes from the", a("Transparency International", href = "https://www.transparency.org/en/cpi"),
+                             "which compiles the CPI Index."),
                      tags$li("GDP per capita, inequality, government spending, 
-                              and poverty all came from the", a("The World Bank", href = "https://data.worldbank.org"), 
+                              and poverty all come from the", a("The World Bank", href = "https://data.worldbank.org"), 
                              "dataset. The inequality data is a lot spottier than the other three indicators."),
                      tags$li("Bureaucratic remuneration and public campaign finance 
-                              come from the vdem dataset, accessed through the vdem 
-                              package."),
-                     tags$li("Economic freedom"),
-                     tags$li("Spending on infrastructure")
-                 )
-                   ))
-        
-        
-    )
-)
+                              come from the", a("vdem", href = "https://www.v-dem.net/en/"), 
+                              "dataset, accessed through the vdem package."),
+                     tags$li("Infrastructure spending comes from the", a("Infralatam", href = "http://home.infralatam.info/"), 
+                             "dataset."))),
+                 h3("About Me"),
+                 p("I am a junior at Harvard College studying History and Government, with a focus on public policy and diplomacy in Latin America. You can reach me at", 
+                   a("kfoster@college.harvard.edu", href = "mailto:kfoster@college.harvard.edu"),"."
+                 ))))
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -62,8 +98,10 @@ server <- function(input, output) {
         select(country, CPI, geometry) %>%
             ggplot(aes(geometry = geometry)) +
             geom_sf(aes(fill = CPI)) +
-            scale_fill_distiller(palette = "Reds")
-    }, height = 400, width = 600)
+            scale_fill_distiller(palette = "Reds") +
+            theme(legend.position = "left") +
+            theme_void()
+        })
     
     plot_geom <- reactive({
         switch(input$geom,
@@ -74,8 +112,11 @@ server <- function(input, output) {
     })
     
     output$plot <- renderPlot({
-        ggplot(final_data, aes(.data[[input$x]], CPI)) +
-            plot_geom()})
+        final_data %>%
+            drop_na(.data[[input$x]]) %>%
+            ggplot(aes(.data[[input$x]], CPI)) +
+            plot_geom()
+        })
 }
 
 shinyApp(ui = ui, server = server)
--- regressions.R
+++ regressions.R
@@ -1,21 +1,97 @@
 library(rpart)
 library(rpart.plot)
 library(randomForest)
+library(neuralnet)
+library(tidymodels)
+library(rstanarm)
 
-stan_glm(data = final_data,
-         CPI ~ `GDP Per Capita`*`Gini`*`Poverty Rate` + `Public Campaign Finance` + `Bureaucratic Remuneration` + `Government Spending` + `Infrastructure Spending`,
-         refresh = 0) %>%
-  print(digits = 5)
+final_data <- final_data %>%
+  drop_na()
 
-robust_model <- rlm(CPI ~ `GDP Per Capita`,
-                    data = final_data)
+final_split <- initial_split(final_data, prop = 0.9)
+final_train <- training(final_split)
+final_test  <- testing(final_split)
+final_folds <- vfold_cv(data = final_split %>% as_tibble(), v = 5)
 
-tree <- rpart(CPI ~ `GDP Per Capita` + `Gini` + `Poverty Rate` + `Public Campaign Finance` + `Bureaucratic Remuneration` + `Government Spending` + `Infrastructure Spending`,
-              data = final_data,
+error <- function(model){
+  predict(model, newdata = final_test) %>%
+    as_tibble() %>%
+    bind_cols(final_test %>% 
+                select(CPI)) %>%
+    rmse(truth = CPI, estimate = value)
+}
+
+stan_model_1 <- stan_glm(CPI ~ gdp_pc + gini + govt_spending + poverty_rate + bur_rem +
+                           pcf + infra_spend, 
+                         data = final_train, 
+         refresh = 0)
+
+stan_1_error <- error(stan_model_1)
+
+stan_model_2 <- stan_glm(data = final_train,
+                         CPI ~ gdp_pc + gini*poverty_rate + govt_spending * infra_spend +
+                           bur_rem + pcf, 
+                         refresh = 0)
+
+stan_2_error <- error(stan_model_2)
+
+stan_model_3 <- stan_glm(CPI ~ log_gdp + gini + govt_spending + poverty_rate + bur_rem +
+                           pcf + infra_spend, 
+                         data = final_train, 
+                         refresh = 0)
+
+stan_3_error <- error(stan_model_3)
+
+simple_stan <- stan_glm(data = final_train, 
+                        CPI ~ gdp_pc + bur_rem + poverty_rate,
+                        refresh = 0)
+
+stan_4_error <- error(simple_stan)
+
+bind_rows(stan_1_error, stan_2_error, stan_3_error, stan_4_error, 
+          tree_error, rf_error, rf_log_error)
+
+tree <- rpart(CPI ~ gdp_pc + gini + govt_spending + poverty_rate + bur_rem +
+                pcf + infra_spend,
+              data = final_train,
               cp = 0.1)
+
+tree_error <- error(tree)
 
 rpart.plot(tree,
            type = 1)
 
-rf <- randomForest(CPI ~ `GDP Per Capita` + `Gini` + `Poverty Rate` + `Public Campaign Finance` + `Bureaucratic Remuneration`,
-                   data = final_data)
+rf <- randomForest(CPI ~ gdp_pc + gini + govt_spending + poverty_rate + bur_rem +
+                     pcf + infra_spend, 
+                   data = final_train)
+
+rf_log <- randomForest(CPI ~ log_gdp + gini + govt_spending + poverty_rate + bur_rem +
+                     pcf + infra_spend, 
+                   data = final_train)
+
+rf_error <- error(rf)
+rf_log_error <- error(rf_log)
+
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
+}
+
+normalized_train <- final_train %>%
+  select(-country) %>%
+  normalize(.) 
+
+normalized_test <- final_test %>%
+  select(-country) %>%
+  normalize(.) 
+
+nn <- neuralnet(CPI ~ gdp_pc + gini + govt_spending + poverty_rate + bur_rem +
+                  pcf + infra_spend,
+          data = normalized_train,
+          hidden = c(5, 2),
+          linear.output = TRUE)
+
+predict(nn, newdata = normalized_test) %>%
+  as_tibble() %>%
+  bind_cols(final_test %>% 
+              select(CPI)) %>%
+  rmse(truth = CPI, estimate = V1)
